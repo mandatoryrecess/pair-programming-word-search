1017c7209acbdeb205b2223ba86f898504d92988
Ross Clamp <81119910+mandatoryrecess@users.noreply.github.com>
2021-06-02 17:11:59 -0700

    Update wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,18 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+    const horizontalJoin = letters.map(ls => ls.join(''));
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) return true;
     }
-}
 
-module.exports = wordSearch
+    let emptyString = "";
+    for (let y = 0; y < letters[0].length; y++) {
+        for (let x = 0; x < letters.length; x++) {
+            emptyString += letters[x][y];
+            
+        }      
+    }
+    if (emptyString.includes(word)) {
+        return true;
+    } 
+    return false; 
+}
